3f388ff5382196b4a6d81ee67998671cca6cda5a
Natpon <25012514amonwan@gmail.com>
2026-01-16 19:17:45 +0700

    ttt

--- src/practical/p01/index.ts
+++ src/practical/p01/index.ts
@@ -1,1 +1,39 @@
-export function getPostalAddress() {}
+import axios from "axios";
+interface Output{
+    id: number;
+    name: string;
+    phone: string;
+    address: Address;
+}
+interface Geo{
+    lat: string;
+    lng: string;
+}
+interface Company{
+    name: string;
+    catchPhrase: string;
+    bs: string;
+}
+interface Address {
+    street:string;
+    suite: string;
+    city: string;
+    zibcode: string;
+    geo: Geo;
+}
+interface DATA {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+    address: Address;
+    company: Company;
+}
+export ansync function getPostalAddress() {
+    const res = await axios.get("https://jsonplaceholder.typicode.com/users");
+    const data = res.data;
+    const D : Output[]=[];
+    for(const dd of data){
+        D += dd;
+    }
+    console.log(D);
